2a7fa961bdad340163d1d0087316589105e1f24a
shubham_9195 <shubham@servitor.in>
2019-11-18 13:11:03 +0530

    Updates

---
+++ precache-manifest.37aa017d621832af75debf19afbeda22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da65b70cdce3d209b8b1605659ea217a",
+    "url": "/2048-game/index.html"
+  },
+  {
+    "revision": "a016ebae4be74a2de28f",
+    "url": "/2048-game/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "763f93d5057699b0eba9",
+    "url": "/2048-game/static/js/2.48dce93a.chunk.js"
+  },
+  {
+    "revision": "a016ebae4be74a2de28f",
+    "url": "/2048-game/static/js/main.3ab43d01.chunk.js"
+  },
+  {
+    "revision": "5a1d2629b23a54192220",
+    "url": "/2048-game/static/js/runtime-main.a8051a5b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/2048-game/static/media/logo.25bf045c.svg"
+  }
+]);
